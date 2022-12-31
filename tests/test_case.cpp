a0307939d6efbdd909cca4784f99b83f29264591
YISHEN CAI <yisen614@gmail.com>
2022-12-31 11:42:02 +0800

    update case

--- tests/test_case.cpp
+++ tests/test_case.cpp
@@ -3,6 +3,14 @@
 //
 #include "gtest/gtest.h"
 
+
+void func(int num) {
+    if (num / 2 > 0) {
+        func(num / 2);
+    }
+    printf("%d,", num);
+}
+
 TEST(TestCaseGroup, equalCase) {
     ASSERT_EQ(1, 2 - 1);
 }
@@ -10,4 +18,17 @@ TEST(TestCaseGroup, equalCase) {
 
 TEST(TestCaseGroup, notEqualCase) {
     ASSERT_TRUE(1 != 2);
+}
+
+
+TEST(TestCaseGroup, recursionCase) {
+    func(10);
+}
+
+
+
+TEST(TestCaseGroup, arrayCase) {
+    int a[] = {1, 2, 3, 4};
+    printf("first: %d\n", *(a));
+    printf("second: %d\n", *(a + 1));
 }
